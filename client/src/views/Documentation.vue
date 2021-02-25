658e9767ed472ed9a8cabab5248463f0f35f2b91
Jared Finn <jaredhfinn@gmail.com>
2021-02-25 15:32:09 -0500

    Routing views

---
+++ client/src/views/Documentation.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="notification is-warning">
+  <button class="delete"></button>
+  <h1 class="title is-1">This is Documentation Page</h1>
+  Primar lorem ipsum dolor sit amet, consectetur
+  adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur.
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
