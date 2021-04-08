705abfae9d0959ca870117673bd8979dc9f4e8da
Jared Finn <jaredhfinn@gmail.com>
2021-04-08 15:11:59 -0400

    client and server are communicating

---
+++ client/src/models/myFetch.js
@@ -0,0 +1,10 @@
+/*
+    A wrapper for the native fetch api
+
+*/
+
+export const API_ROOT = "http://localhost:3000/";
+
+export function api(url){
+    return fetch(API_ROOT + url).then(x => x.json());
+}
