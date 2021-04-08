7f225c81a05fba89923f770896805f7b2dac7041
Jared Finn <jaredhfinn@gmail.com>
2021-04-08 15:28:56 -0400

    using dotenv to direct api calls

--- client/src/models/myFetch.js
+++ client/src/models/myFetch.js
@@ -3,7 +3,7 @@
 
 */
 
-export const API_ROOT = "http://localhost:3000/";
+export const API_ROOT = process.env.VUE_APP_API_ROOT;
 
 export function api(url){
     return fetch(API_ROOT + url).then(x => x.json());
